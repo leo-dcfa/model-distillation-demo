bbc96fa9bdc092c9eb08d95afe05b50dc95b2204
Leo <leo-dcfa>
2026-05-18 14:04:14 +1000

    save constants

--- src/constants.py
+++ src/constants.py
@@ -5,4 +5,4 @@ DEFAULT_STUDENT: Final[str] = "Qwen/Qwen2.5-0.5B"
 DEFAULT_EPOCHS: Final[int] = 2000
 DEFAULT_OUTPUT_DIR: Final[str] = "./distilled_student"
 DEFAULT_TEACHER: Final[str] = "Qwen/Qwen2.5-3B-Instruct"
-DEFAULT_NUMBER_EXAMPLES: Final[int] = 2
+DEFAULT_NUMBER_EXAMPLES: Final[int] = 500
